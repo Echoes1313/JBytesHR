8b0564d935bc548772e340a35366c26630afb4f5
Echoes1313 <Danek1313@gmail.com>
2012-12-11 23:03:06 -0500

    Made JBytesHR much more object-oriented

--- JBytesHR.js
+++ JBytesHR.js
@@ -3,76 +3,86 @@
      *
      *
      */
-    obj.JBytesHR = function() {
-        var decimalPlaces = 2;
+    obj.JBytesHR = function(decimalPlaces, useFullWord, useBase10) {
+        
+        this.usingBase10    = useBase10 === true;
+        this.usingFullWord     = useFullWord === true;
+        this.decimalPlaces  = (decimalPlaces % 1 === 0 ? decimalPlaces : 2)
+        var converter       = this;
+        
+        function ByteUnit(intPower, abbrv, word) {
+            this.base2  = Math.pow(1024, intPower);
+            this.base10 = Math.pow(1000, intPower);
+            this.abbrv  = abbrv;
+            this.word   = word;
+        }
 
-        var kiloBytes = 1024, kiloBytesBase10 = 1000;
-        var megaBytes = Math.pow(1024, 2), megaBytesBase10 = Math.pow(1000, 2);
-        var gigaBytes = Math.pow(1024, 3), gigaBytesBase10 = Math.pow(1000, 3);
-        var teraBytes = Math.pow(1024, 4), teraBytesBase10 = Math.pow(1000, 4);
-        var pedaBytes = Math.pow(1024, 5), pedaBytesBase10 = Math.pow(1000, 5);
-        var exaBytes = Math.pow(1024, 6), exaBytesBase10 = Math.pow(1000, 6);
+        ByteUnit.prototype.getBytes = function() {
+            if (converter.usingBase10) {
+                return this.base10;
+            } else {
+                return this.base2;
+            }
+        }
 
-        var kbAbbrv = 'kB', kbWord = 'kilobytes';
-        var mbAbbrv = 'MB', mbWord = 'megabytes';
-        var gbAbbrv = 'GB', gbWord = 'gigabytes';
-        var tbAbbrv = 'TB', tbWord = 'terabytes';
-        var pbAbbrv = 'PB', pbWord = 'pedabytes';
-        var ebAbbrv = 'EB', ebWord = 'exabytes';
+        ByteUnit.prototype.getText = function() {
+            if (converter.usingFullWord) {
+                return this.word;
+            } else {
+                return this.abbrv;
+            }
+        }
+
+        var unitLib = {
+             b: new ByteUnit(0, 'b',  'bytes'),
+            kb: new ByteUnit(1, 'kB', 'kilobytes'),
+            mb: new ByteUnit(2, 'mB', 'kilobytes'),
+            gb: new ByteUnit(3, 'gB', 'gigabytes'),
+            tb: new ByteUnit(4, 'tB', 'terabytes'),
+            pb: new ByteUnit(5, 'pB', 'pedabytes'),
+            eb: new ByteUnit(6, 'eB', 'exabytes')
+        }
 
         /**
-         * Shorthand method for converting bytes to the largest unit greater than one possible,
-         *    to the default decimal places.
+         * Shorthand/convenience method for convertBytesToHumanReadable
          * @param incomingBytes The integer of bytes to be converted.
          */
         this.convert = function(incomingBytes) {
-            return this.convertBytesToHumanReadable(incomingBytes, decimalPlaces, true, false);
+            return this.convertBytesToHumanReadable(incomingBytes);
         };
         
         /**
-         * Main method for converting a number of bytes to more readable units.
+         * Main method for converting a number of bytes to the largest unit greater than one possible.
          * @param incomingBytes The integer of bytes to be converted.
-         * @param desiredDecimalPlaces How many decimal places to preserver. Extra zeroes will be truncated.
-         * @param useAbbreviations Whether or not to output 'kB' or 'kilobytes'
-         * @param useBase10 Whether or not to use powers of 10 instead of 2. (see http://en.wikipedia.org/wiki/Megabyte#Definition)
          */
-        this.convertBytesToHumanReadable = function(incomingBytes, desiredDecimalPlaces, useAbbreviations, useBase10) {
-            var kB = (useBase10===true ? kiloBytesBase10 : kiloBytes);
-            var mB = (useBase10===true ? megaBytesBase10 : megaBytes);
-            var gB = (useBase10===true ? gigaBytesBase10 : gigaBytes);
-            var tB = (useBase10===true ? teraBytesBase10 : teraBytes);
-            var pB = (useBase10===true ? pedaBytesBase10 : pedaBytes);
-            var eB = (useBase10===true ? exaBytesBase10    : exaBytes);
+        this.convertBytesToHumanReadable = function(incomingBytes) {
 
-            var mFactor = Math.pow(10, (!desiredDecimalPlaces ? decimalPlaces : desiredDecimalPlaces));
-            var resultString = '';
+            var mFactor = Math.pow(10, this.decimalPlaces);
+            var resultKey = '';
 
             if (isNaN(incomingBytes) || incomingBytes < 0) {
-                resultString = 'Invalid size argument: ' + incomingBytes;
-            } else if (incomingBytes < kB) {
-                resultString = incomingBytes + ' ' + (useAbbreviations===true?'B':'bytes');
-            } else if (incomingBytes >= kB && incomingBytes < mB) {
-                var value = Math.round(incomingBytes/kB*mFactor)/mFactor;
-                resultString = value + ' ' + (useAbbreviations===true?'kB':'kilobytes');
-            } else if (incomingBytes >= mB && incomingBytes < gB) {
-                var value = Math.round(incomingBytes/mB*mFactor)/mFactor;
-                resultString = value + ' ' + (useAbbreviations===true?'MB':'megabytes');
-            } else if (incomingBytes >= gB && incomingBytes < tB) {
-                var value = Math.round(incomingBytes/gB*mFactor)/mFactor;
-                resultString = value + ' ' + (useAbbreviations===true?'GB':'gigabytes');
-            } else if (incomingBytes >= tB && incomingBytes < pB) {
-                var value = Math.round(incomingBytes/tB*mFactor)/mFactor;
-                resultString = value + ' ' + (useAbbreviations===true?'TB':'terabytes');
-            } else if (incomingBytes >= pB && incomingBytes < eB) {
-                var value = Math.round(incomingBytes/pB*mFactor)/mFactor;
-                resultString = value + ' ' + (useAbbreviations===true?'PB':'pedabytes');
-            } else if (incomingBytes >= eB) {
-                var value = Math.round(incomingBytes/eB*mFactor)/mFactor;
-                resultString = value + ' ' + (useAbbreviations===true?'EB':'exabytes');
+                return resultString = 'Invalid size argument: ' + incomingBytes;
+            } else if (incomingBytes < unitLib.kb.getBytes()) {
+                resultKey = 'b';
+            } else if (incomingBytes >= unitLib.kb.getBytes() && incomingBytes < unitLib.mb.getBytes()) {
+                resultKey = 'kb';
+            } else if (incomingBytes >= unitLib.mb.getBytes() && incomingBytes < unitLib.gb.getBytes()) {
+                resultKey = 'mb';
+            } else if (incomingBytes >= unitLib.gb.getBytes() && incomingBytes < unitLib.tb.getBytes()) {
+                resultKey = 'gb';
+            } else if (incomingBytes >= unitLib.tb.getBytes() && incomingBytes < unitLib.pb.getBytes()) {
+                resultKey = 'tb';
+            } else if (incomingBytes >= unitLib.pb.getBytes() && incomingBytes < unitLib.eb.getBytes()) {
+                resultKey = 'pb';
+            } else if (incomingBytes >= unitLib.eb.getBytes()) {
+                resultKey = 'eb';
             } else {
                 throw 'Invalid input: '+incomingBytes;
             }
-            return resultString;
+            
+            var value = Math.round(incomingBytes/unitLib[resultKey].getBytes()*mFactor) / mFactor;
+            var text = unitLib[resultKey].getText();
+            return value + ' ' + text;
         };
     };
 } (this));
