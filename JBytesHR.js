e264cb2e2150c344f500a9648317ecd5f7e43468
Echoes1313 <Danek1313@gmail.com>
2013-01-08 21:13:43 -0500

    Add option to return numeric value instead of just a string

--- JBytesHR.js
+++ JBytesHR.js
@@ -1,14 +1,18 @@
 (function(obj) {
     /**
-     *
-     *
+     * A JBytesHR object's constructor takes three parameters:
+     * @param decimaPlaces  (integer) The desired decimal precision outputted
+     * @param useFullWord   (boolean) Whether you want the full word instead of the abbreviation ("Megabytes" vs. "mB")
+     * @param useBase10     (boolean) Whether you wish to use the base 10 definition of the higher units (10^3 for kB instead of 2^10). See http://www.wikipedia.org/en/Megabyte
+     * @param returnInteger (boolean) If you want a number value returned (with no descriptor string afterwards), set this to true.
      */
-    obj.JBytesHR = function(decimalPlaces, useFullWord, useBase10) {
+    obj.JBytesHR = function(decimalPlaces, useFullWord, useBase10, returnNumber) {
         
         this.usingBase10    = useBase10 === true;
-        this.usingFullWord     = useFullWord === true;
+        this.usingFullWord  = useFullWord === true;
+        this.returnNumber  = returnNumber === true;
         this.decimalPlaces  = (decimalPlaces % 1 === 0 ? decimalPlaces : 2)
-        var converter       = this;
+        var self       = this;
         
         function ByteUnit(intPower, abbrv, word) {
             this.base2  = Math.pow(1024, intPower);
@@ -18,7 +22,7 @@
         }
 
         ByteUnit.prototype.getBytes = function() {
-            if (converter.usingBase10) {
+            if (self.usingBase10) {
                 return this.base10;
             } else {
                 return this.base2;
@@ -26,7 +30,7 @@
         }
 
         ByteUnit.prototype.getText = function() {
-            if (converter.usingFullWord) {
+            if (self.usingFullWord) {
                 return this.word;
             } else {
                 return this.abbrv;
@@ -44,24 +48,12 @@
         }
 
         /**
-         * Shorthand/convenience method for convertBytesToHumanReadable
-         * @param incomingBytes The integer of bytes to be converted.
+         *  Private method which accepts the incoming byte value and returns a key for the unitLib map.
          */
-        this.convert = function(incomingBytes) {
-            return this.convertBytesToHumanReadable(incomingBytes);
-        };
-        
-        /**
-         * Main method for converting a number of bytes to the largest unit greater than one possible.
-         * @param incomingBytes The integer of bytes to be converted.
-         */
-        this.convertBytesToHumanReadable = function(incomingBytes) {
-
-            var mFactor = Math.pow(10, this.decimalPlaces);
-            var resultKey = '';
-
+        var getKey = function(incomingBytes) {
+            var resultKey;
             if (isNaN(incomingBytes) || incomingBytes < 0) {
-                return resultString = 'Invalid size argument: ' + incomingBytes;
+                throw 'Invalid JBytesHR size argument exception: '+incomingBytes;
             } else if (incomingBytes < unitLib.kb.getBytes()) {
                 resultKey = 'b';
             } else if (incomingBytes >= unitLib.kb.getBytes() && incomingBytes < unitLib.mb.getBytes()) {
@@ -77,12 +69,41 @@
             } else if (incomingBytes >= unitLib.eb.getBytes()) {
                 resultKey = 'eb';
             } else {
-                throw 'Invalid input: '+incomingBytes;
+                throw 'Invalid JBytesHR size argument exception: '+incomingBytes;
             }
-            
-            var value = Math.round(incomingBytes/unitLib[resultKey].getBytes()*mFactor) / mFactor;
-            var text = unitLib[resultKey].getText();
-            return value + ' ' + text;
+            return resultKey;
+        }
+
+        /**
+         * Shorthand method for convertBytesToHumanReadable
+         * @param incomingBytes The integer of bytes to be converted.
+         */
+        this.convert = function(incomingBytes) {
+            return this.convertBytesToHumanReadable(incomingBytes);
+        };
+        
+        /**
+         * Main method for converting a number of bytes to the largest unit greater than one possible.
+         * @param incomingBytes The integer of bytes to be converted.
+         */
+        this.convertBytesToHumanReadable = function(incomingBytes) {
+
+            var mFactor = Math.pow(10, this.decimalPlaces);
+            var resultKey = '';
+
+            var resultKey = getKey(parseInt(incomingBytes, 10));
+
+            if (resultKey===false) {
+                return resultString = 'Invalid JBytesHR size argument exception: ' + incomingBytes;
+            }
+
+            if (this.returnInteger===true) {
+              return incomingBytes/(unitLib[resultKey].getBytes()*1.0);
+            } else {
+              var value = Math.round(incomingBytes/unitLib[resultKey].getBytes()*mFactor) / mFactor;
+              var text = unitLib[resultKey].getText();
+              return value + ' ' + text;
+            }
         };
     };
 } (this));
